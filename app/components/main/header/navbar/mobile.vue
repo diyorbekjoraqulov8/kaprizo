7cfc71471de1f75bae640ce58af2ebda245c5cea
diyorbek <diyorbekjoraqulov8@gmail.com>
2025-08-08 21:43:26 +0500

    some changes

--- app/components/main/header/navbar/mobile.vue
+++ app/components/main/header/navbar/mobile.vue
@@ -15,7 +15,7 @@ onClickOutside(target, () => emits('close'))
       class="p-4 fixed top-0 left-0 w-11/12 min-[400px]:w-3/5 h-dvh bg-white z-50"
   >
     <div class="flex items-center justify-between gap-4 mb-4">
-      <LazySharedLogo />
+      <LazySharedLogo theme="light" />
 
       <button @click="() => emits('close', $event)" class="text-xl p-2 rounded bg-light text-gray-light border">
         <IconClose />
