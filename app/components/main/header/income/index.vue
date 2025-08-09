ec05da47fb78853a7e545634485f09082d5f8833
diyorbek <diyorbekjoraqulov8@gmail.com>
2025-08-09 20:35:19 +0500

    income

--- app/components/main/header/income/index.vue
+++ app/components/main/header/income/index.vue
@@ -3,57 +3,78 @@ import { useI18n } from "vue-i18n";
 import {scrollToSmooth} from "~/composables";
 
 const {t} = useI18n()
+
+//&::before {
+//  content: "";
+//  position: absolute;
+//  width: 100%;
+//  height: 600px;
+//  top: 50%;
+//  left: -50%;
+//  transform: translateY(-50%);
+//  background: url("gift.png") no-repeat center center;
+//  background-size: contain;
+//}
 </script>
 
 <template>
-  <div id="income" class="income h-[calc(100dvh-100px)]">
-    <div class="container h-full flex items-center justify-center lg:justify-between">
-      <div class="hidden lg:flex w-1/4"/>
-      <div class="w-full lg:w-3/4 text-center relative">
-        <p class="income_gift text-9xl">{{ t('income.gift') }}</p>
-        <p class="income_two text-6xl md:text-8xl">{{ t('income.text_two') }}</p>
-        <p class="income_three text-2xl md:text-4xl">{{ t('income.text_three') }}</p>
-        <p class="income_five text-4xl md:text-7xl">{{ t('income.text_four') }}</p>
+  <div id="income" class="income h-[calc(80dvh-100px)] lg:h-[calc(100dvh-100px)]">
+    <div class="container h-full flex items-center justify-center">
+      <div class="income_img w-1/6 sm:w-1/3 lg:w-1/2 h-full flex items-center">
+        <LazyNuxtImg
+            src="gift.png"
+            alt="Gift"
+            class="flex w-full max-xl:-translate-x-1/2 max-xl:scale-150 max-lg:scale-[2] max-sm:scale-[6] max-sm:-translate-x-full"
+        />
+      </div>
 
-        <SharedButton
-            class="mb-2"
-            size="extra-large"
-            type="primary"
-            @click="() => scrollToSmooth('partner')"
-        >
-          {{ t('income.install_app') }}
-        </SharedButton>
-
-        <div>
-          <LazyIconTop class="text-[#d8a654] text-2xl text-center mx-auto"/>
-
-          <p class="income_six">{{ t('income.text_six') }}</p>
-          <p class="income_six">{{ t('income.text_six_2') }}</p>
-        </div>
+      <div class="w-5/6 sm:w-1/2 text-center">
+        <h2 class="income_gift text-7xl lg:text-8xl">{{ t('income.gift') }}</h2>
+        <h2 class="income_gift text-7xl lg:text-8xl">{{ t('income.gift_two') }}</h2>
+        <h3 class="income_two text-4xl lg:text-5xl">{{ t('income.text') }}</h3>
+        <h3 class="income_two text-4xl lg:text-5xl">{{ t('income.text_two') }}</h3>
       </div>
     </div>
+
+<!--    <div class="container h-full flex items-center justify-center lg:justify-between">-->
+<!--      <div class="hidden lg:flex w-1/4"/>-->
+<!--      <div class="w-full lg:w-3/4 text-center relative">-->
+<!--        <p class="income_gift text-9xl">{{ t('income.gift') }}</p>-->
+<!--        <p class="income_two text-6xl md:text-8xl">{{ t('income.text_two') }}</p>-->
+<!--        <p class="income_three text-2xl md:text-4xl">{{ t('income.text_three') }}</p>-->
+<!--        <p class="income_five text-4xl md:text-7xl">{{ t('income.text_four') }}</p>-->
+
+<!--        <SharedButton-->
+<!--            class="mb-2"-->
+<!--            size="extra-large"-->
+<!--            type="primary"-->
+<!--            @click="() => scrollToSmooth('partner')"-->
+<!--        >-->
+<!--          {{ t('income.install_app') }}-->
+<!--        </SharedButton>-->
+
+<!--        <div>-->
+<!--          <LazyIconTop class="text-[#d8a654] text-2xl text-center mx-auto"/>-->
+
+<!--          <p class="income_six">{{ t('income.text_six') }}</p>-->
+<!--          <p class="income_six">{{ t('income.text_six_2') }}</p>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
 
 <style scoped>
 .income {
-  background: url("@/assets/images/income-bg.png") no-repeat center left;
-  background-size: cover;
-  @media (max-width: 1023px) {
-    background: none;
-  }
+  &_img {
 
+  }
   &_gift {
     font-family: "Great Vibes", sans-serif;
-    position: absolute;
-    top: -12%;
-    left: 50%;
-    transform: translate(-50%, -50%);
     color: white;
   }
   &_two {
     font-family: "Bebas Neue", sans-serif;
-    font-weight: 500;
     text-transform: uppercase;
     z-index: -999 !important;
     line-height: 1;
